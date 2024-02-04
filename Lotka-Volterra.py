6b45d43b21d7dfb7bce5e7ed891f01f8169a4030
MATEBOOKOFSHK\shkong <shkong36@outlook.com>
2024-02-04 08:19:53 +0800

    更新无性别变化种群微分方程函数

--- Lotka-Volterra.py
+++ Lotka-Volterra.py
@@ -20,11 +20,21 @@ def S(P):
     '''sex ratio'''
     return 11/(50*(1+np.exp(S_a*(P+S_b))))+0.56
 
+
 def derivative(t,X):
     M,F,P = X
     s=S(P)
     dMdt = -r_m*M+B(M,F,P)*S(P)
     dFdt = -r_f*F+B(M,F,P)*(1-S(P))
+    dPdt = P*(r_p-a*M-a*F)#-alpha*P**2
+    return np.array([dMdt,dFdt,dPdt])
+
+
+def deri_nosex(t,X):
+    M,F,P = X
+    s=S(P)
+    dMdt = -r_m*M+B(M,F,P)*0.5
+    dFdt = -r_f*F+B(M,F,P)*0.5
     dPdt = P*(r_p-a*M-a*F)#-alpha*P**2
     return np.array([dMdt,dFdt,dPdt])
 
@@ -63,4 +73,3 @@ plt.grid()
 plt.plot(t,M/(M+F),'r')
 plt.title("sex ratio",fontsize=15)
 plt.show()
-
